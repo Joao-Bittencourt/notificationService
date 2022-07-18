1bab799320a45a604a5c1fc6c4dd4835fb316ed6
Joao Bittencourt <joaogsbittencourt@gmail.com>
2022-07-17 23:06:26 -0300

    first commit

--- src/tests/notificationTest.php
+++ src/tests/notificationTest.php
@@ -10,5 +10,10 @@ class notificationTest extends TestCase {
     public function test_add_notification_to_queue() {
         $this->markTestIncomplete();
     }
+    
+    public function test_assert_true() {
+        $this->assertTrue(true);
+    }
+    
 
 }
